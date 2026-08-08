e7403c022ef275c98dbe9bff991cf37541712088
fenildhanani <fenildhanani28@gmail.com>
2026-08-08 18:30:30 +0530

    patternc2

---
+++ lactur8/p2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int r;
+    int c;
+    int n;
+    cout<<"enter your number : ";
+    cin>>n;
+
+    for(r=1;r<=n;r++){
+
+        for(c=1;c<=n-r;c++)
+        {
+            cout<<"  ";
+        }
+        for(c=1;c<=r;c++)
+        {
+        for(c=r-1;c=1;c--)
+        {
+            cout<<c<<" ";
+        }
+    }
+        cout<<endl;
+    }
+}
