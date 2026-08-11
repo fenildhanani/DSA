b2879e444134250a0d3f5cdd7b6b0e37c1322e2b
fenildhanani <fenildhanani28@gmail.com>
2026-08-11 17:49:04 +0530

    break

---
+++ table.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"enter your number :";
+    cin>>n;
+    int i =1;
+
+    while(i<=10){
+        cout<<n*i<<endl;
+        i++;
+    }
+}
